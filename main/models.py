a5e74a82f20a92e35cecbb56060b314a81b1aace
d-T0x <dzaki.azzam06@gmail.com>
2026-09-22 00:41:28 +0700

    feat(project): add give star to projects

--- main/models.py
+++ main/models.py
@@ -1,4 +1,6 @@
 import uuid
+
+from django.contrib.auth.models import User 
 from django.db import models
 
 
@@ -43,6 +45,9 @@ class Project(models.Model):
     created_at = models.DateField()
     programs = models.TextField()
     link = models.URLField()
+    starred_by = models.ManyToManyField(
+            User, related_name="starred_projects", blank=True
+    )
     def __str__(self):
         return self.title
 
--- main/urls.py
+++ main/urls.py
@@ -14,6 +14,7 @@ from main.views import (
     register,
     login_user,
     logout_user,
+    toggle_star,
 )
 
 app_name = "main"
@@ -28,6 +29,7 @@ urlpatterns = [
     path("api/project/", get_projects_json, name="get_projects_json"),
     path("api/experience/", get_experiences_json, name="get_experiences_json"),
     path("project/<uuid:project_id>/delete/",delete_project,name="delete_project"),
+    path("projects/<uuid:project_id>/star/", toggle_star, name="toggle_star"),
     path("experience/<uuid:experience_id>/delete/",delete_experience,name="delete_experience"),
     path("register/", register, name="register"),
     path("login/", login_user, name="login"),
